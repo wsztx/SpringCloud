30567a2d70781e7a9bc180eb8a69ec7649e5b16d
zhoutianxiang <1390958241@qq.com>
2021-05-10 19:05:00 +0800

    cloud框架模板提交

---
+++ springcloud-provider/src/main/java/com/universe/springcloud/provider/SpringcloudProviderApplication.java
@@ -0,0 +1,13 @@
+package com.universe.springcloud.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderApplication.class, args);
+    }
+
+}
